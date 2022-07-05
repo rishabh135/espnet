e9be764e4242f68ae91a2460b6b5cfd76e92ce34
Rishabh Gupta <rgupta@gruss-3.nancy.grid5000.fr>
2022-07-05 12:54:10 +0200

    again not workink, just stashing commig for sanctitiy

--- egs2/librispeech_100/asr1/run_without_adversarial.sh
+++ egs2/librispeech_100/asr1/run_without_adversarial.sh
@@ -18,7 +18,7 @@ asr_tag=conformer_lr2e-3_warmup15k_amp_nondeterministic
 # project_name="june_15_freezing_encoder_asr_lmt_trigram_with_adv"
 
 
-project_name="nancy_july_5_adv"
+project_name="nancy_july_4_without_adv"
 
 data_dd=/srv/storage/talc2@talc-data2.nancy/multispeech/calcul/users/rgupta/fresh_libri_100/${project_name}/data # determines all the files creating folder as in the data folder
 
@@ -47,7 +47,7 @@ inference_config=/home/rgupta/dev/espnet/egs2/librispeech_100/asr1/conf/decode_a
 ###################################################################################################################################################################################################
 ###################################################################################################################################################################################################
 ###################################################################################################################################################################################################
-./asr.sh \
+./asr_without_adversarial.sh \
     --skip_data_prep false \
     --skip_train false \
     --skip_eval false \
@@ -70,3 +70,4 @@ inference_config=/home/rgupta/dev/espnet/egs2/librispeech_100/asr1/conf/decode_a
     --lm_train_text "${data_dd}/${train_set}/text" \
     --bpe_train_text "${data_dd}/${train_set}/text" "$@" 
     
+
